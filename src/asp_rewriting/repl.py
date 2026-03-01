fbb06742fe7ec990d73c142fee972a3cc18fceb8
gianzav <gianlucagianluca18@gmail.com>
2026-03-01 13:36:15 +0100

    basic repl

---
+++ src/asp_rewriting/repl.py
@@ -0,0 +1,99 @@
+from parser import RuleParser
+from matcher import RuleMatcher
+from writer import RuleWriter
+import parsy
+import logging
+import argparse
+from dataclasses import dataclass
+
+
+def peek(x):
+    y = x.pop()
+    x.add(y)
+    return y
+
+
+@dataclass
+class Context:
+    parser = RuleParser()
+    matcher = RuleMatcher(parser)
+    writer = RuleWriter()
+    rules = []
+    buffer = set()
+
+
+def repl(ctx: Context):
+
+    while True:
+        inp = input("::> ").strip()
+        if inp != "":
+            evaluate(inp, ctx)
+
+
+def evaluate(input_, ctx: Context):
+    parser = ctx.parser
+    matcher = ctx.matcher
+    writer = ctx.writer
+    rules = ctx.rules
+    buffer = ctx.buffer
+    done = set()
+
+    try:
+        rule = parser.parse(input_)
+        rules.append(rule)
+    except:
+        buffer |= {input_}
+
+        while len(buffer) > 0:
+            inp = peek(buffer)
+            match = False
+            for rule in rules:
+                try:
+                    bindings = matcher.get_bindings(rule.pattern, inp)
+                    match = True
+                    buffer.remove(inp)
+                    for skeleton in rule.skeletons:
+                        rewritten = writer.write(skeleton, bindings)
+                        buffer.add(rewritten)
+                        # logging.info(rewritten)
+                    break
+                except Exception as e:
+                    logging.debug(
+                        f"No match for pattern {str(rule.pattern)} on '{inp}'"
+                    )
+                    # raise e
+
+            if not match:
+                done.add(inp)
+                buffer.remove(inp)
+
+        for r in done:
+            print(r)
+
+
+def make_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", nargs="?", type=str)
+    parser.add_argument("-i", "--interactive", action="store_true")
+    return parser
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    argparser = make_parser()
+    args = argparser.parse_args()
+    ctx = Context()
+
+    if args.filename:
+        with open(args.filename, "r") as f:
+            code = f.readlines()
+            for line in code:
+                evaluate(line, ctx)
+        if args.interactive:
+            repl(ctx)
+    else:
+        repl(ctx)
+
+
+if __name__ == "__main__":
+    main()
