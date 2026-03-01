71982f1de1bd23ffa1d0de6569113279089831df
gianzav <gianlucagianluca18@gmail.com>
2026-03-01 13:33:45 +0100

    fixed parsing of extension in skeleton variable expansion

--- src/asp_rewriting/parser.py
+++ src/asp_rewriting/parser.py
@@ -117,9 +117,10 @@ def skeleton_rule():
 
     # extraction of the variables inside a match. E.g. with ?body, $body/vars is
     # expanded to the variables appearing inside whatever was matched in ?body
-    skeleton_reference_variable_vars = (
-        dollar >> regex(r"[a-z]+[a-z0-9]*") << string("/vars")
-    ).map(SkeletonVariableVarExpansion)
+    skeleton_reference_variable_vars = seq(
+        dollar >> regex(r"[a-z]+[a-z0-9]*") << string("/vars"),
+        variable_extension.optional(""),
+    ).combine(lambda var, ext: SkeletonVariableVarExpansion(var, ext))
 
     # numeric variable that denotes a newly generated symbol, e.g. $1
     skeleton_numeric_variable = seq(
